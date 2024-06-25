3decda3c7a4d10d947ac186e3b1d1205afa51f51
sound2862 <62818790+sound2862@users.noreply.github.com>
2024-06-25 17:21:27 +0900

    Add files via upload

---
+++ CSharpBasic2/Triangle.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //삼각형의 넓이를 구하는 수식을 작성하라.
+            Console.Write("세로 : ");
+            int height = int.Parse(Console.ReadLine());
+
+            Console.Write("가로 : ");
+            int width = int.Parse(Console.ReadLine());
+
+            double result = (width * height)/2.0;
+
+            Console.WriteLine("결과는? : " + result);
+        }
+    }
+}
